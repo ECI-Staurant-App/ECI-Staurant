37865114e63eef155a252cbaeba9a833d2550a90
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-02-21 08:46:06 -0500

    Añadiendo archivos de configuracion

---
+++ src/main/java/edu/eci/arsw/EciStaurant/EciStaurantApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.EciStaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciStaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciStaurantApplication.class, args);
+	}
+
+}
