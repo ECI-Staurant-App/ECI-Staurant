4c6fc562c79a98f040ae29578f140e1dbe2d5f8f
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-03-06 15:41:27 -0500

    Ya conecta db

--- src/main/java/edu/eci/arsw/EciStaurant/EciStaurantApplication.java
+++ src/main/java/edu/eci/arsw/EciStaurant/EciStaurantApplication.java
@@ -2,8 +2,10 @@ package edu.eci.arsw.EciStaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"cistaurant"})
 public class EciStaurantApplication {
 
 	public static void main(String[] args) {
--- src/main/java/edu/eci/arsw/EciStaurant/model/Platillo.java
+++ src/main/java/edu/eci/arsw/EciStaurant/model/Platillo.java
@@ -1,4 +1,11 @@
 package edu.eci.arsw.EciStaurant.model;
 
+/*import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity*/
 public class Platillo {
 }
