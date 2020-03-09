5229f6b2dbec80d4fa41074e9c6c66a9d38145c2
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-03-09 18:02:01 -0500

    Modified

--- src/main/java/edu/eci/arsw/EciStaurant/controllers/EcistaurantController.java
+++ src/main/java/edu/eci/arsw/EciStaurant/controllers/EcistaurantController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping( value = "/")
 public class EcistaurantController {
 
-
-
-        @RequestMapping(method = RequestMethod.GET)
         public String getMensaje() {
             String ans = "Estamos Trabajando en ECI-STAURANT ;)";
             return ans;
